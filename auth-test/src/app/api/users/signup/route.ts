a1e9a0ee01ce9ceadd2ed1e189f6f049032e0d24
JulieGunawan <julie.gunawan@protonmail.com>
2025-03-05 13:03:25 -0700

    create an api for post user

---
+++ auth-test/src/app/api/users/signup/route.ts
@@ -0,0 +1,42 @@
+import {connect} from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try{
+    const reqBody = await request.json();
+    const {username, email, password} = reqBody
+
+    //check if user is exists
+    const user = await User.findOne({email});
+    if (user){
+      return NextResponse.json({error: "User already exists"}, {status: 400})
+    }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //create new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword
+    })
+
+    const savedUser = await newUser.save();
+
+    return NextResponse.json({
+      message: "User created successfully",
+      success: true,
+      savedUser
+    },{status: 201})
+  } catch (err) {
+    const error = err as Error
+    return NextResponse.json({error: error.message}, {status: 500})
+  }
+}
--- auth-test/src/app/login/page.tsx
+++ auth-test/src/app/login/page.tsx
@@ -1,7 +1,54 @@
-export default function Login() {
+"use client";
+import Link from "next/link";
+import React from "react";
+import {useRouter} from "next/navigation";
+import {axios} from "axios";
+
+
+
+export default function LoginPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  })
+  const onLogin = async() => {
+    
+  }
+  
   return (
-    <div className="flex">
-      <h1 className="text-2xl">login</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-2">
+      <div className="w-full max-w-md" >
+        <h2 className="text-2xl text-center">Login</h2>
+        <hr className="mb-4"/>
+        <div className="w-full">
+          <label htmlFor="email" className="text-left block">email</label>
+          <input
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="email"
+            type="email"
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            placeholder="email"
+          />
+        </div>
+        <div className="w-full">
+          <label htmlFor="password" className="text-left block">password</label>
+          <input
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="password"
+            type="password"
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            placeholder="password"
+          />
+        </div>
+        <div className="w-full flex justify-between">
+          <button onClick={onLogin} className="px-4 py-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 hover:bg-black hover:text-white">
+            Login
+          </button>
+          <Link href="/signup" className="px-4 py-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 hover:bg-black hover:text-white">Sign Up</Link>
+        </div>
+      </div>
     </div>
   );
 }
